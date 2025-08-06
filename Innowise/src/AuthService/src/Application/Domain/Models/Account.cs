3540da3de1c64314a3d1f7e413163374fb133ef2
DESKTOP-HRNVRMV\romar <romanrudenka2004@gmail.com>
2025-08-06 13:48:26 +0300

    simple models for auth

--- Innowise/src/AuthService/src/Application/Domain/Models/Account.cs
+++ Innowise/src/AuthService/src/Application/Domain/Models/Account.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Models
 {
-    internal class Accounts
+    internal class Account
     {
+        public string Id { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmPassword { get; set; }
+        
+
+        public Account(string email, string password, string confirmPassword)
+        {
+            Id = Guid.NewGuid().ToString();
+            Email = email;
+            Password = password;
+            ConfirmPassword = confirmPassword;
+        }
     }
 }
